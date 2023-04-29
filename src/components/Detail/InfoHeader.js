9a64f66f9151cebc4f27c6cfd26ad62d32db9d53
Jiayu Ran <jiayuran@pc-138-150.customer.ask4.lan>
2023-04-29 14:38:35 +0100

    change tab-spaces to 2

--- src/components/Detail/InfoHeader.js
+++ src/components/Detail/InfoHeader.js
@@ -2,7 +2,7 @@
  * @Author: Jiayu Ran
  * @Date: 2023-04-20 09:54:17
  * @LastEditors: Jiayu Ran
- * @LastEditTime: 2023-04-26 09:36:28
+ * @LastEditTime: 2023-04-29 14:37:14
  * @Description: Detail page header
  */
 import React, { useState } from "react";
@@ -10,68 +10,68 @@ import { FaTwitter, FaLink, FaShareAlt, FaChevronLeft } from "react-icons/fa";
 import "../../styles/Detail/InfoHeader.css";
 
 function InfoHeader(props) {
-    let placeName = props.placeName;
-    //share function
-    const [showShareBox, setShowShareBox] = useState(false);
-    // toggle share box
-    function handleShareClick() {
-        if (showShareBox === true) {
-            setShowShareBox(false);
-        } else {
-            setShowShareBox(true);
-        }
+  let placeName = props.placeName;
+  //share function
+  const [showShareBox, setShowShareBox] = useState(false);
+  // toggle share box
+  function handleShareClick() {
+    if (showShareBox === true) {
+      setShowShareBox(false);
+    } else {
+      setShowShareBox(true);
     }
-    function handleCloseClick() {
-        setShowShareBox(false);
-    }
-    // back to previous page
-    function handleBack() {
-        window.history.back();
-    }
-    // copy link to paste board
-    function copyLink() {
-        let url = window.location.href;
-        navigator.clipboard.writeText(url);
-        alert("Link copied!");
-        setShowShareBox(false);
-    }
-    // share by email
-    function shareToTwitter() {
-        let url = "https://twitter.com/intent/tweet?text=";
-        let text = `This is a interesting place called ${placeName}, here is link: ${window.location.href}`;
-        url += text;
-        window.open(url);
-    }
-    return (
-        <div>
-            <div className="info_header">
-            
-            <div className="backBtn" onClick={handleBack}>
-                <FaChevronLeft style={{fontSize: "22px", color: "#919191"}} />
-            </div>
-            <div className="placeName">
-                {placeName}
-            </div>
-            <div className="shareBtn" onClick={handleShareClick}>
-                <FaShareAlt style={{fontSize: "22px", color: "#919191"}} />
-            </div>
-            </div>
-            {showShareBox && (
-                <div className="shareBox">
-                    <div onClick={shareToTwitter}>
-                        <FaTwitter className="emailIcon" />
-                        <span className="iconSpan">Twitter</span>
-                    </div>
-                    <div onClick={copyLink}>
-                        <FaLink className="linkIcon" />
-                        <span className="iconSpan">Copy link</span>
-                    </div>
-                    <div className="closeBtn" onClick={handleCloseClick}>
-                        Close
-                    </div>
-                </div>
-            )}
+  }
+  function handleCloseClick() {
+    setShowShareBox(false);
+  }
+  // back to previous page
+  function handleBack() {
+    window.history.back();
+  }
+  // copy link to paste board
+  function copyLink() {
+    let url = window.location.href;
+    navigator.clipboard.writeText(url);
+    alert("Link copied!");
+    setShowShareBox(false);
+  }
+  // share by email
+  function shareToTwitter() {
+    let url = "https://twitter.com/intent/tweet?text=";
+    let text = `This is a interesting place called ${placeName}, here is link: ${window.location.href}`;
+    url += text;
+    window.open(url);
+  }
+  return (
+    <div>
+      <div className="info_header">
+
+        <div className="backBtn" onClick={handleBack}>
+          <FaChevronLeft style={{ fontSize: "22px", color: "#919191" }} />
         </div>
-    )
+        <div className="placeName">
+          {placeName}
+        </div>
+        <div className="shareBtn" onClick={handleShareClick}>
+          <FaShareAlt style={{ fontSize: "22px", color: "#919191" }} />
+        </div>
+      </div>
+      {showShareBox && (
+        <div className="shareBox">
+          <div onClick={shareToTwitter}>
+            <FaTwitter className="emailIcon" />
+            <span className="iconSpan">Twitter</span>
+          </div>
+          <div onClick={copyLink}>
+            <FaLink className="linkIcon" />
+            <span className="iconSpan">Copy link</span>
+          </div>
+          <div className="closeBtn" onClick={handleCloseClick}>
+            Close
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 export default InfoHeader;
--- src/components/Detail/InfoPic.js
+++ src/components/Detail/InfoPic.js
@@ -2,7 +2,7 @@
  * @Author: Jiayu Ran
  * @Date: 2023-04-20 09:54:17
  * @LastEditors: Jiayu Ran
- * @LastEditTime: 2023-04-26 09:37:00
+ * @LastEditTime: 2023-04-29 14:37:29
  * @Description: Detail page picture slider
  */
 import React, { useState } from "react";
@@ -14,31 +14,31 @@ import "../../styles/Detail/InfoPic.css";
 //view pictures
 
 const InfoPic = (props) => {
-    let images = [];
-    // get all photos' references
-    const photos = props.photos;
-    if (photos && photos.length) {
-        for (let i = 0; i < photos.length; i ++) {
-            let picUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photos[i].photo_reference}&key=${config['MAP_API_KEY']}`;
-            images.push(picUrl);
-        }
+  let images = [];
+  // get all photos' references
+  const photos = props.photos;
+  if (photos && photos.length) {
+    for (let i = 0; i < photos.length; i++) {
+      let picUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photos[i].photo_reference}&key=${config['MAP_API_KEY']}`;
+      images.push(picUrl);
     }
-    const [index, setIndex] = useState(0);
-    const handlePrev = () => {
-        setIndex((index - 1 + images.length) % images.length);
-    }
-    const handleNext = () => {
-        setIndex((index + 1) % images.length);
-    }
-    return (
-        <div className="carousel">
-            <div className="pics-container">
-                <img src={images[index]} alt="" className="pics" />
-                <img src={leftButton} className="leftBtn" onClick={handlePrev} />
-                <img src={rightButton} className="rightBtn" onClick={handleNext} />
-            </div>
-        </div>
-    );
+  }
+  const [index, setIndex] = useState(0);
+  const handlePrev = () => {
+    setIndex((index - 1 + images.length) % images.length);
+  }
+  const handleNext = () => {
+    setIndex((index + 1) % images.length);
+  }
+  return (
+    <div className="carousel">
+      <div className="pics-container">
+        <img src={images[index]} alt="" className="pics" />
+        <img src={leftButton} className="leftBtn" onClick={handlePrev} />
+        <img src={rightButton} className="rightBtn" onClick={handleNext} />
+      </div>
+    </div>
+  );
 }
 
 export default InfoPic;
